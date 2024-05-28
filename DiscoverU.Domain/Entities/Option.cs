96fbf156a44bb4f3a44fca30602278e7067da2d6
Ftm-btl <88613684+Ftm-btl@users.noreply.github.com>
2024-05-28 21:41:05 +0300

    .

--- DiscoverU.Domain/Entities/Option.cs
+++ DiscoverU.Domain/Entities/Option.cs
@@ -13,6 +13,6 @@ namespace DiscoverU.Domain.Entities
         public string Text { get; set; }
         public int QuestionId { get; set; }
         public Question Question { get; set; }
-        public Question Questio { get; set; }
+        
     }
 }
