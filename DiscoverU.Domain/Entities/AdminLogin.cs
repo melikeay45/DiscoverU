ca69dd8faf8b1ed312cd04bb1af69c650ff03ecd
Melike Aydın <melike.ay45@gmail.com>
2024-05-28 22:13:12 +0300

    [Features]:Added AdminLogin.

---
+++ DiscoverU.Domain/Entities/AdminLogin.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DiscoverU.Domain.Entities
+{
+    public class AdminLogin
+    {
+        public string UserName { get; set; }
+        public string Password { get; set; }
+    }
+}
