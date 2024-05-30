39ad6aba00085de62a91da3e5697f600540f4241
avocadoseedd <95234795+avocadoseedd@users.noreply.github.com>
2024-05-30 11:52:35 +0300

    [Feature]:Added IResponseService,IsurveyService,IUserService

---
+++ DiscoverU.Application/Services/IResponseService.cs
@@ -0,0 +1,18 @@
+﻿using DiscoverU.Application.Dtos.ResponseDto;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DiscoverU.Application.Services
+{
+    public interface IResponseService
+    {
+        List<GetResponseDto> GetAll();
+        GetResponseDto GetById(Guid id);
+        void Add(AddReponseDto addResponseDto);
+        void Update(UpdateResponseDto updateResponseDto);
+        void Delete(Guid id);
+    }
+}
---
+++ DiscoverU.Application/Services/ISurveyService.cs
@@ -0,0 +1,18 @@
+﻿using DiscoverU.Application.Dtos.SurveyDto;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DiscoverU.Application.Services
+{
+    public interface ISurveyService
+    {
+        List<GetSurveyDto> GetAll();
+        GetSurveyDto GetById(Guid id);
+        void Add(AddSurveyDto addSurveyDto);
+        void Update(UpdateSurveyDto updateSurveyDto);
+        void Delete(Guid id);
+    }
+}
