7b1308f416fc703f9581c8f41ad563f6ef0831ce
Melike Aydın <melike.ay45@gmail.com>
2024-05-30 13:47:23 +0300

    [Feature]: Added tailwind css.

--- DiscoverU.Presentation/tailwind.config.js
+++ DiscoverU.Presentation/tailwind.config.js
@@ -3,7 +3,9 @@ module.exports = {
     theme: {
         extend: {},
     },
-    plugins: [require("daisyui")],
+    plugins: [
+        require('daisyui'),
+    ],
     daisyui: {
         themes: ["cupcake"],
     },
