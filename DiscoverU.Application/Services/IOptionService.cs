6702c5a773d9c2b2f159e4d97666c05116bd23a3
Ftm-btl <88613684+Ftm-btl@users.noreply.github.com>
2024-05-30 11:52:34 +0300

    [Feature]: added IAdminLoginService,IOptionService,IQuestionService

---
+++ DiscoverU.Application/Services/IOptionService.cs
@@ -0,0 +1,18 @@
+﻿using DiscoverU.Application.Dtos.OptionDto;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DiscoverU.Application.Services
+{
+    public interface IOptionService
+    {
+        List<GetOptionDto> GetAll();
+        GetOptionDto GetById(Guid id);
+        void Add(AddOptionDto addOptionDto);
+        void Update(UpdateOptionDto updateOptionDto);
+        void Delete(Guid id);
+    }
+}
