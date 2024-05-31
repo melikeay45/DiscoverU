43325dc0e7bbe4bd275fc5143882593ee406ad01
avocadoseedd <95234795+avocadoseedd@users.noreply.github.com>
2024-05-31 15:51:49 +0300

    [Fixed] Update Dto

--- DiscoverU.Application/Dtos/SurveyDto/UpdateSurveyDto.cs
+++ DiscoverU.Application/Dtos/SurveyDto/UpdateSurveyDto.cs
@@ -12,5 +12,6 @@ namespace DiscoverU.Application.Dtos.SurveyDto
         public Guid Id { get; set; }
         public string Title { get; set; }
         public string Description { get; set; }
+        public string ImageUrl { get; set; }
     }
 }
